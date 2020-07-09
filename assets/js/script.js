2d9b0a88b57fea22c08b94effbd80effbdbb304f
Mark <youremail@gmail.comg>
2020-07-09 14:28:57 -0600

    wrote two functions for last.fm api to get similar songs

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+
+    // Function that gets us our track mbid
+    function getTrackInfo() {
+        var artist = prompt("artist pls")
+        var song = prompt("song pls")
+        var queryURL = `http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=0288ec49437b4cf920a1f4c62e1f1f2a&artist=${artist}&track=${song}&format=json`
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            getSimilarSong(response)
+        })
+    }
+    // function that get our similar song list
+    function getSimilarSong(response) {
+        
+
+        $.ajax({
+            url: "http://ws.audioscrobbler.com/2.0/",
+            method: "GET",
+            data: {
+            method: "track.getsimilar",
+            mbid: response.track.mbid,
+            api_key: "0288ec49437b4cf920a1f4c62e1f1f2a",
+            format: "json",
+            limit: "10"
+        }
+        }).then(function (data) {
+            for (var i = 0; i < data.similartracks.track.length; i++) {
+                console.log(data)
+            console.log(data.similartracks.track[i].name)
+            console.log(data.similartracks.track[i].artist.name)
+            console.log(data.similartracks.track[i].mbid)
+            }
+        })
+    }
+
+
+
+                     
+
+
+
+getTrackInfo();
