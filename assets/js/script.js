6273f7a973b17c8ce77fdfec799c201a6019258a
Maksim Verkhoturov <sd32@protonmail.com>
2020-07-09 15:23:29 -0600

    added genius API lyrics parser integration, linking jQuery in HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
-
 // Function that gets us our track mbid
 function getTrackInfo() {
-    var artist = prompt("artist pls")
-    var song = prompt("song pls")
+    var artist = "the xx";
+    var song = "crystalize";
     var queryURL = `http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=0288ec49437b4cf920a1f4c62e1f1f2a&artist=${artist}&track=${song}&format=json`
     $.ajax({
         url: queryURL,
@@ -39,25 +38,52 @@ function getSimilarSong(response) {
 
 
 
-
-
-getTrackInfo();
-
 // youtube API
 function youtubeSearch(searchQ) {
     $.ajax({
-        type: 'GET',
-        url: 'https://www.googleapis.com/youtube/v3/search',
-        data: {
-            key: 'AIzaSyDcZydHg3A14jzQpXUnM7HElOVHptJIpU0',
-            q: searchQ,
-            part: 'snippet',
-            maxResults: 5,
-            type: 'video',
-            videoEmbeddable: true,
-        }
-    })
+            type: 'GET',
+            url: 'https://www.googleapis.com/youtube/v3/search',
+            data: {
+                key: 'AIzaSyDcZydHg3A14jzQpXUnM7HElOVHptJIpU0',
+                q: searchQ,
+                part: 'snippet',
+                maxResults: 5,
+                type: 'video',
+                videoEmbeddable: true,
+            }
+        })
         .then(function (response) {
             console.log(response);
         });
 }
+
+
+// Genius API
+var regex = new RegExp(`<div class="lyrics">(.|\n)*<!--sse-->(.|\n)*<!--/sse-->`);
+
+function geniusLyricsParser() {
+    $.ajax({
+            url: "https://cors-anywhere.herokuapp.com/https://genius.com/Slopps-my-crib-lyrics",
+            type: "GET",
+            dataType: "html"
+        })
+
+        .then(function (response) {
+            var lyrics = response.match(regex)[0].slice(55, response.match(regex)[0].length - 11).trim();
+            for (var i = 0; lyrics.search("<br>") !== -1; i++) {
+                lyrics = lyrics.replace("<br>", "");
+            }
+            for (var i = 0; lyrics.search("<p>") !== -1; i++) {
+                lyrics = lyrics.replace("<p>", "");
+            }
+            for (var i = 0; lyrics.search("</p>") !== -1; i++) {
+                lyrics = lyrics.replace("</p>", "");
+            }
+            console.log(lyrics);
+
+
+
+        });
+}
+
+geniusLyricsParser();
