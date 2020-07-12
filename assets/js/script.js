2598f4299325283a8fbea80f22437a433070770a
Maksim Verkhoturov <sd32@protonmail.com>
2020-07-12 15:52:28 -0600

    wiring up everything to generate the lyrics and songs and youtube embedded element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Function that gets us our track mbid
+// LastFM API
 const lastFMGetTrackInfo = async (artist, track) => {
     var lastFMSearchResponse = await $.ajax({
         url: "http://ws.audioscrobbler.com/2.0/",
@@ -15,8 +15,6 @@ const lastFMGetTrackInfo = async (artist, track) => {
     console.log(lastFMSearchResponse)
     return lastFMSearchResponse;
 }
-
-
 
 const lastFMGetSimilarTracks = async (lastFMRes) => {
 
@@ -37,24 +35,33 @@ const lastFMGetSimilarTracks = async (lastFMRes) => {
     for (var i = 0; i < lastFMSimilarTracks.similartracks.track.length; i++) {
         similarTracks.push(lastFMSimilarTracks.similartracks.track[i].name + ' by ' + lastFMSimilarTracks.similartracks.track[i].artist.name)
     }
-    console.log(similarTracks)
+
+    console.log(similarTracks);
+
+    return similarTracks;
 }
+// /!LastFM Api
 
 
 
-function youtubeSearch(searchQ) {
-    $.ajax({
-            type: 'GET',
-            url: 'http://youtube-scrape.herokuapp.com/api/search',
-            data: {
-                q: searchQ,
-                page: '1'
-            }
-        })
-        .then(function (response) {
-            console.log(response);
-        });
+
+// YouTube
+const youTubeSearch = async (searchQuery) => {
+    var youTubeSearchResult = await $.ajax({
+        type: 'GET',
+        url: 'https://cors-anywhere.herokuapp.com/http://youtube-scrape.herokuapp.com/api/search',
+        dataType: "json",
+        data: {
+            q: searchQuery,
+            page: '1'
+        }
+    })
+
+    // returning the ID of the first result from YouTube
+    return youTubeSearchResult.results[0].video.id;
 }
+// /! YouTube
+
 
 
 
@@ -62,19 +69,19 @@ function youtubeSearch(searchQ) {
 
 // Searches for songs through geniusAPI
 // Returns a URL of the first result
-const geniusGetSongURLbyName = async (songNameForGenius = "my crib by slopps") => {
+const geniusGetSongURLbyName = async (songName) => {
     // requesting search results from genius.com
     var geniusSearchResponse = await $.ajax({
         url: "https://api.genius.com/search",
         type: "GET",
         data: {
-            q: songNameForGenius,
+            q: songName,
             access_token: "39mbxzJoZqsELd5bHonlLHTdSRSj3vqWGn3pJ8mYRSad_y4K8maYbOKqgle3YeWA"
         }
     })
 
     // TODO DEPLOY remove verbose logging (optionally)
-    console.log("GeniusAPI: ", "Searching for: ", songNameForGenius);
+    console.log("GeniusAPI: ", "Searching for: ", songName);
     console.log("GeniusAPI: ", geniusSearchResponse.response.hits.length, " results found.");
     // TODO TALK always return first only?
     console.log("Only returning the first result.");
@@ -94,13 +101,13 @@ const geniusGetSongURLbyName = async (songNameForGenius = "my crib by slopps") =
 // (made to work only with genius songs webpages)
 // Returns song lyrics as an array
 // where one item of the array equals one line of lyrics
-const geniusGetLyricsBySongURL = async (songURL) => {
+const geniusGetLyricsBySongURL = async (geniusSongUrl) => {
     var regexATagsOpeningFull = new RegExp("<a[\\s\\S]*?>|</a>|<p>|</p>|<!--/", "g");
 
-    // requesting the html page of the songURL
+    // requesting the html page of the geniusSongUrl
     // assigning the response of the call to geniusSearchResponse
     var songHTML = await $.ajax({
-        url: `https://cors-anywhere.herokuapp.com/${songURL}`,
+        url: `https://cors-anywhere.herokuapp.com/${geniusSongUrl}`,
         type: "GET",
         dataType: "html"
     })
@@ -113,43 +120,86 @@ const geniusGetLyricsBySongURL = async (songURL) => {
 
     return songLyricsArr;
 }
+// /!Genius API
 
 
 
-// TODO jsdoc
-const generateLyrics = (lyricsArray) => {
+// Generates similar songs to the one entered
+// takes 2 positional arguments
+// :similar song list as an array (1 song - 1 item)
+// :name of the currently entered song
+const generateSimilarSongs = (similarSongsArray, songName) => {
 
-    var lyricsSection = $("#lyrics-section");
-    lyricsSection.empty();
+    var songListSection = $("#songListSection");
+    songListSection.empty();
 
-    for (item of lyricsArray) {
-        lyricsSection.append($("<span>").attr("class", "lyrics-line").text(item));
+    songListSection.append($("<li>").attr({
+        class: "song-item",
+        id: "enteredSong"
+    }).text(songName));
+
+    for (item of similarSongsArray) {
+        songListSection.append($("<li>").attr("class", "song-item").text(item));
     }
 
 }
 
-// // function for calling lastFM data
-// (async function () {
 
-// })()
+// Generates lyrics on the page
+// takes 1 positional argument
+// :lyrics as an array (1 line - 1 item)
+const generateLyrics = (geniusLyricsArray) => {
+    var lyricsSection = $("#lyricsSection");
+    lyricsSection.empty();
+
+    for (item of geniusLyricsArray) {
+        lyricsSection.append($("<span>").attr("class", "lyrics-line").html(item));
+    }
+
+}
 
 
-
-
-
-
-$("#searchButton").on("click", (async () => {
+const generateAllSongElements = async () => {
     var artistUserInput = $("#artistName");
     var songUserInput = $("#songName");
     var artistUser = artistUserInput.val().trim();
     var songUser = songUserInput.val().trim();
+    var youTubeIframeSection = $("#iframe-container");
 
-    // lastFM calls
+    // async lastFM request for song information
+    // everything else is based on that response
     var songInfo = await lastFMGetTrackInfo(artistUser, songUser);
-    lastFMGetSimilarTracks(songInfo);
+    // async lastFM request for similar songs
+    var lastFMSimilarSongsArray = await lastFMGetSimilarTracks(songInfo);
+    // async YouTube-Scraper request for search results from YouTube
+    var youTubeId = await youTubeSearch(songInfo.track.artist.name + " " + songInfo.track.name);
+    // async Genius request for song URL
+    var geniusSongURL = await geniusGetSongURLbyName(songInfo.track.artist.name + " " + songInfo.track.name);
+    // async Genius request for lyrics of that song
+    var geniusLyricsArray = await geniusGetLyricsBySongURL(geniusSongURL);
 
-    // genius calls
-    var songURL = await geniusGetSongURLbyName(artistUser + " " + songUser);
-    var lyricsArray = await geniusGetLyricsBySongURL(songURL);
-    // generateLyrics(lyricsArray);
-}));
+    // Generating lastFM similar songs on the page
+    generateSimilarSongs(lastFMSimilarSongsArray, songInfo.track.artist.name + " by " + songInfo.track.name);
+
+    // Generating Genius lyrics on the page
+    generateLyrics(geniusLyricsArray);
+
+    // Generating YouTube embed on the page
+    youTubeIframeSection.empty();
+    youTubeIframeSection.append($("<iframe>").attr({
+        src: `https://www.youtube-nocookie.com/embed/${youTubeId}?modestbranding=1&showinfo=0&rel=0&iv_load_policy=3&fs=0&color=white&autohide=0`,
+        frameborder: "0",
+        gesture: "media",
+        allow: "autoplay; encrypted-media",
+        allowfullscreen: ""
+    }))
+}
+
+
+
+
+// Search Button on click event
+$("#searchButton").on("click", () => {
+    event.preventDefault();
+    generateAllSongElements();
+});
