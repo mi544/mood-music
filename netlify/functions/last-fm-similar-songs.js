f0a7e544455a1353dc48fe58a285c2de4cf1f949
Maksim Verkhoturov <sd32@protonmail.com>
2021-05-09 02:51:45 -0600

    adds params check and moves env vars to the top

--- netlify/functions/last-fm-similar-songs.js
+++ netlify/functions/last-fm-similar-songs.js
@@ -1,8 +1,18 @@
 const axios = require('axios')
+const { LASTFM_API_KEY } = process.env
 
 exports.handler = async (event) => {
   try {
     const { artist, track } = event.queryStringParameters
+    if (!artist || !track) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          status: 'error',
+          message: '`artist` or `track` parameter missing',
+        }),
+      }
+    }
 
     const { data: similarSongsData } = await axios.get(
       'https://ws.audioscrobbler.com/2.0/',
@@ -10,7 +20,7 @@ exports.handler = async (event) => {
         params: {
           method: 'track.getsimilar',
           autocorrect: '1',
-          api_key: process.env.LASTFM_API_KEY,
+          api_key: LASTFM_API_KEY,
           artist,
           track,
           limit: '30',
--- netlify/functions/last-fm-song-info.js
+++ netlify/functions/last-fm-song-info.js
@@ -1,14 +1,24 @@
 const axios = require('axios')
+const { LASTFM_API_KEY } = process.env
 
 exports.handler = async (event) => {
   try {
     const { artist, track } = event.queryStringParameters
+    if (!artist || !track) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          status: 'error',
+          message: '`artist` or `track` parameter missing',
+        }),
+      }
+    }
 
     const { data: songData } = await axios.get('https://ws.audioscrobbler.com/2.0/', {
       params: {
         method: 'track.getInfo',
         autocorrect: '1',
-        api_key: process.env.LASTFM_API_KEY,
+        api_key: LASTFM_API_KEY,
         artist,
         track,
         format: 'json',
